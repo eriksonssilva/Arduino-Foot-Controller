0548d2f8a99985994fd0c9b3996f59f9a5db2f2b
eriksonssilva <43449566+eriksonssilva@users.noreply.github.com>
2018-09-20 17:32:30 +0000

    Update reper.h

--- Foot Controler 1/reper.h
+++ Foot Controler 1/reper.h
@@ -1,3 +1,5 @@
 #include <Arduino.h> 
 
-String reper[] = {}; // Inside this string you can store the Songs your setlist, so they'll show up whenever you step the defined button
+String reper[] = {}; 
+/*Inside this string you can store the Songs of your setlist, so they'll show up on the
+LCD display whenever you step the defined button*/
